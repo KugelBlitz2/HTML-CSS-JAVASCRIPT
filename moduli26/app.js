d933eb491f171d92fca69f07510bce0368f9bb62
KugelBlitz2 <galica.orik@gmail.com>
2025-09-16 20:42:39 +0200

    1

---
+++ moduli26/app.js
@@ -0,0 +1,40 @@
+console.log("Bismila alhamdulila");
+
+const x = 20;
+
+const _value = "reyy";
+
+const a = 50 + 80;
+
+const e = 50;
+
+const f = 90;
+
+const q = e + f
+
+console.log(q);
+
+const v = 908;
+
+const t = 10;
+
+const u = 4;
+
+const ry = t%u
+
+const z = 50;
+const y = z**2;
+
+console.log(y);
+
+const g = 50;
+
+const yu = 50;
+
+const tu = "80";
+
+console.log(yu === tu);
+
+const emir = "jfhfdhd";
+
+console.log(`Hello from ${emir}`);
